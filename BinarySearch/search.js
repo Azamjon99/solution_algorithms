1a026ad171b2c9c4d7848292e03f36bc3568b5e3
Uktamov Azamjon <azamjon.o@ssd.uz>
2022-06-17 21:26:50 +0500

    recursive search  added

--- BinarySearch/search.js
+++ BinarySearch/search.js
@@ -1,13 +1,13 @@
-function binarySearchRecursive(n, arr) {
+function binarySearchRecursive( arr, target) {
     let mid = Math.floor(arr.length / 2);
-  if (arr.length === 1 && arr[0] != n) {
+  if (arr.length === 1 && arr[0] != target) {
       return false;
     }
-    if (n === arr[middle]) {
+    if (target === arr[middle]) {
       return true;
-    } else if (n < arr[middle]) {
-      return binarySearchRecursive(n, arr.slice(0, middle));
-    } else if (n > arr[middle]) {
-      return binarySearchRecursive(n, arr.slice(middle));
+    } else if (target < arr[middle]) {
+      return binarySearchRecursive( arr.slice(0, middle), target);
+    } else if (target > arr[middle]) {
+      return binarySearchRecursive( arr.slice(middle), target);
     }
 }
