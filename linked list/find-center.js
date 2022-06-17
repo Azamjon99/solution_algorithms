ec61cffb75e4ccbb9016ccd17b66100f83e18d65
Uktamov Azamjon <azamjon.o@ssd.uz>
2022-06-17 20:58:16 +0500

    markazni topish added

---
+++ linked list/find-center.js
@@ -0,0 +1,15 @@
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function findCenter(head){
+    let slow = head, fast = head
+    while(fast !== null && fast.next !== null){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return slow
+
+}
