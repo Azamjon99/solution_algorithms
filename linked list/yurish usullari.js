71b05c5290ed7942125bff598ab9db159346f5e5
Uktamov Azamjon <azamjon.o@ssd.uz>
2022-06-17 20:48:28 +0500

    yurish usullar added

--- linked list/yurish usullari.js
+++ linked list/yurish usullari.js
@@ -1,9 +1,19 @@
-function printList(node) 
+const linkedList = {
+    val: 5,
+    next: {
+      val: 3,
+      next: {
+        val: 10,
+        next: null,
+      },
+    },
+  };
+function printList(linkedList) 
 { 
-    while (node != null) 
+    while (linkedList != null) 
     { 
-        console.log(node.data)
-        node = node.next; 
+        console.log(linkedList.val)
+        linkedList = linkedList.next; 
     } 
 
 } 
